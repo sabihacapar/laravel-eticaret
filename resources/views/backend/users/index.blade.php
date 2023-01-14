5e8d6f182fc11aeeb5f91cec282fb4f7268f6315
sabihacpr <sbhacpr10@gmail.com>
2023-01-14 18:01:18 +0300

    temaları düzenleme -parçalama işlemi

--- resources/views/backend/users/index.blade.php
+++ resources/views/backend/users/index.blade.php
@@ -1,196 +1,81 @@
+@extends('backend.shared.backend_theme')
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+@section('title',"Kullanıcı Modülü")
+ 
+@section('subtitle',"Kullanıcılar")
 
-    <title>Dashboard Template Page</title>
+@section('btn_url',url("/users/create"))
+@section('btn_label',"yeni ekle")
+@section('btn_icon',"plus")
+ 
+@section('content')
+<table class="table table-striped table-sm">
+  <thead>
+    <tr>
+      <th>Sıra No</th>
+      <th>Ad Soyad</th>
+      <th>E-Posta</th>
+      <th>Durum</th>
+      <th>İşlemler</th>
+    </tr>
+  </thead>
+  <tbody>
+    @if(count($users)>0)
+    @foreach ($users as $user)
+        
+    <tr>
+      <td>{{ $loop->iteration }}</td>
+      <td>{{ $user->name }}</td>
+      <td>{{ $user->email }}</td>
+      <td>
+       
+        @if($user->is_active == 1)
+        <span class="badge bg-success">Aktif</span>
+        @else
+        <span class="badge bg-danger">Pasif</span>
+        @endif
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/dashboard/">
 
-    <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+      </td>
+      <td>
+        <ul class="nav float-start">
+          <li class="nav-item">
+            <a href="{{url("/users/$user->user_id/edit") }}" class="nav-link text-black">
+              <span data-feather="edit"></span>
+              Güncelle
+            </a>
 
-    <!-- Custom styles for this template -->
-    <link href="{{ asset("css/dashboard.css") }}" rel="stylesheet">
-
-  </head>
-  <body>
-    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Laravel E-Ticaret</a>
-      <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-      <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Çıkış</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link list-item-delete text-black"
+               href="{{url("/users/$user->user_id")}}">
+                <span data-feather="trash-2"></span>
+                Sil
+            </a>
         </li>
-      </ul>
-    </nav>
+          <li class="nav-item">
+            <a href="{{url("/users/$user->user_id/change-password")}}" class="nav-link text-black">
+              <span data-feather="lock"></span>
+               Şifre Değiştir
+            </a>
 
-    <div class="container-fluid">
-      <div class="row">
-        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-          <div class="sidebar-sticky">
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link active" href="#">
-                  <span data-feather="home"></span>
-                  Yönetim Paneli <span class="sr-only">(current)</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/users">
-                  <span data-feather="file"></span>
-                  Kullanıcılar
-                </a>
-              </li>
-             
-            </ul>
-
-            
-            
-          </div>
-        </nav>
-
-        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-            <h1 class="h2">Yönetim Paneli</h1>
-            
-          </div>
-
-
-          <h2>Kullanıcılar</h2>
-          <div class="btn-tootbar mb-2 mb-md-0">
-            <div class="btn-group mb-2">
-              <a href="/users/create" class="btn btn-sm btn-outline-danger">YEni Ekle</a>
-            </div>
-          </div>
-          <div class="table-responsive">
-            <table class="table table-striped table-sm">
-              <thead>
-                <tr>
-                  <th>Sıra No</th>
-                  <th>Ad Soyad</th>
-                  <th>E-Posta</th>
-                  <th>Durum</th>
-                  <th>İşlemler</th>
-                </tr>
-              </thead>
-              <tbody>
-                @if(count($users)>0)
-                @foreach ($users as $user)
-                    
-                <tr>
-                  <td>{{ $loop->iteration }}</td>
-                  <td>{{ $user->name }}</td>
-                  <td>{{ $user->email }}</td>
-                  <td>
-                   
-                    @if($user->is_active == 1)
-                    <span class="badge bg-success">Aktif</span>
-                    @else
-                    <span class="badge bg-danger">Pasif</span>
-                    @endif
-
-
-                  </td>
-                  <td>
-                    <ul class="nav float-start">
-                      <li class="nav-item">
-                        <a href="{{url("/users/$user->user_id/edit") }}" class="nav-link text-black">
-                          <span data-feather="edit"></span>
-                          Güncelle
-                        </a>
-
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link list-item-delete text-black"
-                           href="{{url("/users/$user->user_id")}}">
-                            <span data-feather="trash-2"></span>
-                            Sil
-                        </a>
-                    </li>
-                      <li class="nav-item">
-                        <a href="{{url("/users/$user->user_id/change-password")}}" class="nav-link text-black">
-                          <span data-feather="lock"></span>
-                           Şifre Değiştir
-                        </a>
-
-                      </li>
-                      
-                    </ul>
-                  </td>
-                  
-                </tr>
-                @endforeach
-
-                @else
-                <tr>
-                    <td colspan="5">
-                        <p class="text-center">Herhangi bir kullanıcı bulunamadı</p>
-                    </td>
-                    
-                    
-                  </tr>
-                @endif
-              </tbody>
-            </table>
-          </div>
-        </main>
-      </div>
-    </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <!-- Icons -->
-    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
-    <script>
-      feather.replace()
-    </script>
-
-    <!-- Graphs -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.7.1/dist/Chart.min.js"></script>
-    <script>
-      var ctx = document.getElementById("myChart");
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-          datasets: [{
-            data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
-            lineTension: 0,
-            backgroundColor: 'transparent',
-            borderColor: '#007bff',
-            borderWidth: 4,
-            pointBackgroundColor: '#007bff'
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: false
-              }
-            }]
-          },
-          legend: {
-            display: false,
-          }
-        }
-      });
-
-
-
+          </li>
+          
+        </ul>
+      </td>
       
-    </script>
-    
-  </body>
-</html>
+    </tr>
+    @endforeach
+
+    @else
+    <tr>
+        <td colspan="5">
+            <p class="text-center">Herhangi bir kullanıcı bulunamadı</p>
+        </td>
+        
+        
+      </tr>
+    @endif
+  </tbody>
+</table>
+@endsection
--- resources/views/backend/users/insert_form.blade.php
+++ resources/views/backend/users/insert_form.blade.php
@@ -1,185 +1,81 @@
+@extends('backend.shared.backend_theme')
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+@section('title',"Kullanıcı Modülü")
+ 
+@section('subtitle',"Yeni Kullanıcı Ekle")
 
-    <title>Dashboard Template Page</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/dashboard/">
-
-    <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="{{ asset("css/dashboard.css") }}" rel="stylesheet">
-  </head>
-
-  <body>
-    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Laravel E-Ticaret</a>
-      <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-      <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Çıkış</a>
-        </li>
-      </ul>
-    </nav>
-
-    <div class="container-fluid">
-      <div class="row">
-        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-          <div class="sidebar-sticky">
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link active" href="#">
-                  <span data-feather="home"></span>
-                  Yönetim Paneli <span class="sr-only">(current)</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ url("/users") }}">
-                  <span data-feather="file"></span>
-                  Kullanıcılar
-                </a>
-              </li>
-             
-            </ul>
-
-            
-            
-          </div>
-        </nav>
-
-        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-            <h1 class="h2">Yönetim Paneli</h1>
-            
-          </div>
+@section('btn_url',url()->previous())
+@section('btn_label',"Geri Dön")
+@section('btn_icon',"arrow-left")
 
 
+ 
+@section('content')
+<form action="{{ url("/users") }}" method="POST" novalidate>
+  @csrf {{-- inputta bir dosya oluşturup oradan değer alır.Bu değere göre de nereden geldiğini ayrıştırır --}}
+   <div class="row">
+     <div class="col-lg-6">
+       
+         <label for="name" class="form-label">Ad Soyad</label>
+         <input type="text" class="form-control" id="name" name="name" value="{{ old("name") }}" placeholder="Ad Soyad Giriniz">
+       @error('name')
+       <span class="text-danger">{{ $message }}</span>
+       @enderror
+                
+     </div>
+     <div class="col-lg-6">
+      
+         <label for="email" class="form-label">E-Posta</label>
+         <input type="email" class="form-control" id="email" name="email" value="{{ old("email") }}" placeholder="Email Giriniz">
+         @error('email')
+         <span class="text-danger">{{ $message }}</span>
+         @enderror
+       
+     </div>
+   </div>
+   <div class="row">
+     <div class="col-lg-6">
+       
+         <label for="password" class="form-label">Şifre</label>
+         <input type="password" class="form-control" id="password" name="password"  placeholder="Şifre Giriniz">
+         @error('password')
+         <span class="text-danger">{{ $message }}</span>
+         @enderror
+       
+     </div>
+     <div class="col-lg-6">
+      
+         <label for="password_confirmation" class="form-label">Şifre Tekrar</label>
+         <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Şifre Tekrar Giriniz">
+         @error('password')
+         <span class="text-danger">{{ $message }}</span>
+         @enderror
+     </div>
+   </div>
+     <div class="row">
+       <div class="col-lg-6">
          
-          <h2>Yeni Kullanıcı Ekle</h2>
-          <div class="table-responsive">
-        <form action="{{ url("/users") }}" method="POST" novalidate>
-         @csrf {{-- inputta bir dosya oluşturup oradan değer alır.Bu değere göre de nereden geldiğini ayrıştırır --}}
-          <div class="row">
-            <div class="col-lg-6">
-              
-                <label for="name" class="form-label">Ad Soyad</label>
-                <input type="text" class="form-control" id="name" name="name" value="{{ old("name") }}" placeholder="Ad Soyad Giriniz">
-              @error('name')
-              <span class="text-danger">{{ $message }}</span>
-              @enderror
-                       
-            </div>
-            <div class="col-lg-6">
-             
-                <label for="email" class="form-label">E-Posta</label>
-                <input type="email" class="form-control" id="email" name="email" value="{{ old("email") }}" placeholder="Email Giriniz">
-                @error('email')
-                <span class="text-danger">{{ $message }}</span>
-                @enderror
-              
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-lg-6">
-              
-                <label for="password" class="form-label">Şifre</label>
-                <input type="password" class="form-control" id="password" name="password"  placeholder="Şifre Giriniz">
-                @error('password')
-                <span class="text-danger">{{ $message }}</span>
-                @enderror
-              
-            </div>
-            <div class="col-lg-6">
-             
-                <label for="password_confirmation" class="form-label">Şifre Tekrar</label>
-                <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Şifre Tekrar Giriniz">
-                @error('password')
-                <span class="text-danger">{{ $message }}</span>
-                @enderror
-            </div>
-          </div>
-            <div class="row">
-              <div class="col-lg-6">
-                
-                
-                  <div class="form-check mt-2 mb-2">
-                    <input class="form-check-input" type="checkbox" id="is_admin" name="is_admin" value="1">
-                    <label class="form-check-label" for="is_admin">
-                      Yetkili Kullanıcı
-                    </label>
-                  </div>
-              </div>
-              <div class="col-lg-6">
-               
-                <div class="form-check mt-2 mb-2">
-                  <input class="form-check-input" type="checkbox" id="is_active" name="is_active" value="1">
-                  <label class="form-check-label" for="is_active">
-                    Kullanıcı Aktif
-                  </label>
-                </div>
-                
-              </div>
-          </div>
-          <div class="row"><div class="col-12">
-            <button type="submit" class="btn btn-success mt-2">Kaydet</button>
-          </div></div>
-        </form>
-          </div>
-        </main>
-      </div>
-    </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <!-- Icons -->
-    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
-    <script>
-      feather.replace()
-    </script>
-
-    <!-- Graphs -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.7.1/dist/Chart.min.js"></script>
-    <script>
-      var ctx = document.getElementById("myChart");
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-          datasets: [{
-            data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
-            lineTension: 0,
-            backgroundColor: 'transparent',
-            borderColor: '#007bff',
-            borderWidth: 4,
-            pointBackgroundColor: '#007bff'
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: false
-              }
-            }]
-          },
-          legend: {
-            display: false,
-          }
-        }
-      });
-    </script>
-  </body>
-</html>
+         
+           <div class="form-check mt-2 mb-2">
+             <input class="form-check-input" type="checkbox" id="is_admin" name="is_admin" value="1">
+             <label class="form-check-label" for="is_admin">
+               Yetkili Kullanıcı
+             </label>
+           </div>
+       </div>
+       <div class="col-lg-6">
+        
+         <div class="form-check mt-2 mb-2">
+           <input class="form-check-input" type="checkbox" id="is_active" name="is_active" value="1">
+           <label class="form-check-label" for="is_active">
+             Kullanıcı Aktif
+           </label>
+         </div>
+         
+       </div>
+   </div>
+   <div class="row"><div class="col-12">
+     <button type="submit" class="btn btn-success mt-2">Kaydet</button>
+   </div></div>
+ </form>
+@endsection
