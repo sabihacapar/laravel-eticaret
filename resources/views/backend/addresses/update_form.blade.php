6f47a1a7ca0d1362bf7e12972c730c99ca1e08a3
sabihacpr <sbhacpr10@gmail.com>
2023-01-30 19:12:25 +0300

    Html Component 2

--- resources/views/backend/addresses/update_form.blade.php
+++ resources/views/backend/addresses/update_form.blade.php
@@ -1,79 +1,53 @@
-
-@extends('backend.shared.backend_theme')
-
-@section('title',"Adres Modülü")
- 
-@section('subtitle',"Adres Güncelle")
-
-@section('btn_url',url()->previous())
-@section('btn_label',"Geri Dön")
-@section('btn_icon',"arrow-left")
-
-
- 
-@section('content')
-        <form action="{{ url("/users/$user->user_id/addresses/$addr->address_id") }}" method="POST" autocomplete="off" novalidate>
-         @csrf {{-- inputta bir dosya oluşturup oradan değer alır.Bu değere göre de nereden geldiğini ayrıştırır --}}
-         @method("PUT")
-         <input type="hidden" name="user_id" value="{{ $user->user_id }}">
-         <input type="hidden" name="address_id" value="{{ $addr->address_id }}">
-
-         <div class="row">
+@extends("backend.shared.backend_theme")
+@section("title","Kullanıcı Modülü")
+@section("subtitle","Kullanıcı Güncelle")
+@section("btn_url",url()->previous())
+@section("btn_label","Geri Dön")
+@section("btn_icon","arrow-left")
+@section("content")
+    <form action="{{url("/users/$user->user_id/addresses/$addr->address_id")}}" method="POST" autocomplete="off"
+          novalidate>
+        @csrf
+        @method("PUT")
+        <input type="hidden" name="user_id" value="{{$user->user_id}}">
+        <input type="hidden" name="address_id" value="{{$addr->address_id}}">
+        <div class="row">
             <div class="col-lg-6">
-              
-                <label for="city" class="form-label">Şehir</label>
-                <input type="text" class="form-control" id="city" name="city" placeholder="Ad Soyad Giriniz" value="{{ old("city",$addr->city) }}">
-              
-                @error('city')
-                <span class="text-danger">{{ $message }}</span>
-                @enderror
-            </div>
-            <div class="col-lg-6">
-             
-                <label for="district" class="form-label">İlçe</label>
-                <input type="text" class="form-control" id="district" name="district" placeholder="İlçe Giriniz" value="{{ old("district",$addr->district) }}">
-                @error('district')
-                <span class="text-danger">{{ $message }}</span>
-                @enderror
-              
-            </div>
-          </div>
-      
-            <div class="row">
-              <div class="col-lg-6">
-                
-                
-                <label for="zipcode" class="form-label">Posta Kodu</label>
-                <input type="text" class="form-control" id="zipcode" name="zipcode" placeholder="İPosta Kodu Giriniz" value="{{ old("zipcode",$addr->district) }}">
-                @error('zipcode')
-                <span class="text-danger">{{ $message }}</span>
-                @enderror
-              
-              </div>
-              <div class="col-lg-6">
-               
-                <div class="form-check mt-2 mb-2">
-                  <input class="form-check-input" type="checkbox" id="is_default" name="is_default" value="1" {{ $addr->is_default == 1 ? "checked" : "" }}>
-                  <label class="form-check-label" for="is_default">
-                    Varsayılan
-                  </label>
+                <div class="mt-2">
+                    <x-input label="Şehir" placeholder="Şehir giriniz" field="city" value="{{$addr->city}}"/>
                 </div>
-                
-              </div>
-          </div>
-          <div class="row">
+            </div>
+            <div class="col-lg-6">
+                <div class="mt-2">
+                    <x-input label="İlçe" placeholder="İlçe giriniz" field="district" value="{{$addr->district}}"/>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="mt-2">
+                    <x-input label="Posta Kodu" placeholder="Posta kodu giriniz" field="zipcode"
+                             value="{{$addr->zipcode}}"/>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="col-lg-6">
+                    <x-checkbox field="is_default" label="Varsayılan" checked="{{$addr->is_default == 1}}"/>
+                </div>
+            </div>
+        </div>
+        <div class="row">
             <div class="col-lg-12">
-              
-              
-              <label for="address" class="form-label">İlçe</label>
-              <textarea type="text" class="form-control" id="address" name="address" placeholder="Adres Giriniz">{{ old("address",$addr->address) }}</textarea>
-              @error('zipcode')
-              <span class="text-danger">{{ $message }}</span>
-              @enderror
-            
-            </div></div>
-          <div class="row"><div class="col-12">
-            <button type="submit" class="btn btn-success mt-2">Kaydet</button>
-          </div></div>
-        </form>
-         @endsection
+                <div class="mt-4">
+                    <x-textarea label="Açık Adres" placeholder="Açık adres giriniz" field="address"
+                                value="{{$addr->address}}"/>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary mt-2"><span data-feather="save"></span> KAYDET</button>
+            </div>
+        </div>
+    </form>
+@endsection
--- resources/views/backend/users/insert_form.blade.php
+++ resources/views/backend/users/insert_form.blade.php
@@ -16,40 +16,31 @@
    <div class="row">
      <div class="col-lg-6">
        
-         <label for="name" class="form-label">Ad Soyad</label>
-         <input type="text" class="form-control" id="name" name="name" value="{{ old("name") }}" placeholder="Ad Soyad Giriniz">
-       @error('name')
-       <span class="text-danger">{{ $message }}</span>
-       @enderror
+         
+      <div class="mt-2">
+       <x-input label="Ad Soyad" placeholder="Ad Soyad Giriniz" field="name"/>
+      </div>
                 
      </div>
      <div class="col-lg-6">
-      
-         <label for="email" class="form-label">E-Posta</label>
-         <input type="email" class="form-control" id="email" name="email" value="{{ old("email") }}" placeholder="Email Giriniz">
-         @error('email')
-         <span class="text-danger">{{ $message }}</span>
-         @enderror
+      <div class="mt-2">
+       <x-input label="Email" placeholder="Email Giriniz" field="email" type="email"/>
+      </div>
        
      </div>
    </div>
    <div class="row">
      <div class="col-lg-6">
-       
-         <label for="password" class="form-label">Şifre</label>
-         <input type="password" class="form-control" id="password" name="password"  placeholder="Şifre Giriniz">
-         @error('password')
-         <span class="text-danger">{{ $message }}</span>
-         @enderror
+      
+      <div class="mt-2">
+       <x-input label="Şifre" placeholder="Şifre Giriniz" field="password" type="password"/>
+      </div>
        
      </div>
      <div class="col-lg-6">
-      
-         <label for="password_confirmation" class="form-label">Şifre Tekrar</label>
-         <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Şifre Tekrar Giriniz">
-         @error('password')
-         <span class="text-danger">{{ $message }}</span>
-         @enderror
+      <div class="mt-2">
+      <x-input label="Şifre Tekrar" placeholder="Şifre Tekrar Giriniz" field="password_confirmation"  type="password"/>
+      </div>
      </div>
    </div>
      <div class="row">
@@ -57,19 +48,17 @@
          
          
            <div class="form-check mt-2 mb-2">
-             <input class="form-check-input" type="checkbox" id="is_admin" name="is_admin" value="1">
-             <label class="form-check-label" for="is_admin">
-               Yetkili Kullanıcı
-             </label>
+             
+        <x-checkbox field="is_admin" label="Yetkili Kullanıcı"/>
+
            </div>
        </div>
        <div class="col-lg-6">
         
          <div class="form-check mt-2 mb-2">
-           <input class="form-check-input" type="checkbox" id="is_active" name="is_active" value="1">
-           <label class="form-check-label" for="is_active">
-             Kullanıcı Aktif
-           </label>
+          
+        <x-checkbox field="is_active" label="Kullanıcı Aktif"/>
+
          </div>
          
        </div>
